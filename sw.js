5631ef979311db58b9e225da6b5dff6d6eec72ad
praisesalau11-ux <praisesalau11@gmail.com>
2026-04-11 14:14:39 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,21 +11,19 @@ const urlsToCache = [
   "logo.png",
   "share.js",
   "share.html",
-  "auth.js"
+  "auth.js",
+  "manifest.json"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
